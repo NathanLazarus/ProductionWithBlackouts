c2d68a1bff2b00297340f854836d774e34970258
Nathan Lazarus <nathanlazarus3@gmail.com>
2023-06-10 01:01:45 -0400

    Exported Graph

--- MakeGraphs.R
+++ MakeGraphs.R
@@ -1,5 +1,5 @@
 tryCatch(find.package('pacman'), error = function(e) install.packages("pacman"))
-pacman::p_load(data.table, ggplot2)
+pacman::p_load(data.table, ggplot2, viridis, cowplot)
 setwd("C:/Users/Nathan/Research/Blackouts")
 data = fread("ProfitsWithAndWithoutGenerators.csv")
 data[
@@ -11,5 +11,16 @@ data[
   )
 ]
 
-ggplot(data) +
-  geom_line(aes(x = A, y = profits, group = type_str))
+to_plot = data[type_str != "Gen_FixedPrice"]
+
+myColors = viridis(n = 3, option = "turbo")
+names(myColors) = unique(to_plot$type)
+colScale = scale_colour_manual(name = "type", values = myColors)
+
+
+ggplot(to_plot) +
+  geom_line(aes(x = A, y = profits, group = type_str, color = type_str)) +
+  theme_cowplot() +
+  colScale
+
+ggsave("ProfitsByProductivity.png")
